1672ad668890ad85e420de38338e92de3cc8b931
Ben Parks <bhp06512@uga.edu>
2024-12-01 00:49:50 -0500

    When the letter at the end of one line and the beginning of another in the alphabet grid was in the correct word, the entire guess_grid line would by cyan. i fixed this by cahnging the __str__ in the letter_2 class so that the end of every string would automatically go back to white.

--- Python_Journey/Wordle/letters_2.py
+++ Python_Journey/Wordle/letters_2.py
@@ -11,5 +11,5 @@ class Letter_2:
         return self.letter
     
     def __str__(self):
-        return f"{self.color}{self.letter}" # to add color to a string you put the color in front of the string itself and it applies when you
+        return f"{self.color}{self.letter}\033[0m" # to add color to a string you put the color in front of the string itself and it applies when you
                                             # apply the __str__ method
